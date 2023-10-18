babd6206241723eca1df476bfc6cbb492b2dcc16
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-10-18 16:35:35 +0200

    Fin démo webpack

--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -1,13 +1,36 @@
 const path = require('node:path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 /** @type {import('webpack').Configuration} */
 const config = {
+  devtool: false,
   entry: './main.ts',
   output: {
     clean: true,
     path: path.resolve(__dirname, '../../dist/client'),
-    filename: 'main.js'
-  }
+    filename: 'bundle.js'
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"],
+    // Add support for TypeScripts fully qualified ESM imports.
+    extensionAlias: {
+     ".js": [".js", ".ts"],
+     ".cjs": [".cjs", ".cts"],
+     ".mjs": [".mjs", ".mts"]
+    }
+  },
+  module: {
+    rules: [
+      // all files with a `.ts`, `.cts`, `.mts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.([cm]?ts|tsx)$/, loader: "ts-loader" }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+  ]
 };
 
 
